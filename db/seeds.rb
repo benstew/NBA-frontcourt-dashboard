d3d3106e7daf8bb92cbfce0b6b0e429b43c1199f
benstew <benstewart2008@gmail.com>
2016-01-28 21:15:39 -0500

    Updated gemfile and included JSON data from Ruby Scraper

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'pry'
+
+File.open('db/json/sportvu_stats.json', 'r') do |file|
+  file.each do |line|
+  all_players = JSON.parse(line)
+
+    all_players.each do |index, player|
+
+      points = (player["points"])
+      frontcourt_touches = player["frontcourt_touches"]
+
+      fc_points_calc = (points/frontcourt_touches).round(4)
+
+
+      Player.create(name: player["name"],
+                    # position:
+                    team: player["team"],
+                    games_played: player["games_played"],
+                    wins: player["wins"],
+                    losses: player["losses"],
+                    minutes: player["minutes"],
+                    points: player["points"],
+                    touches: player["touches"],
+                    time_of_possession: player["time_of_possession"],
+                    seconds_per_touch: player["seconds_per_touch"],
+                    dribbles_per_touch: player["dribbles_per_touch"],
+                    points_per_touch: player["points_per_touch"],
+                    position: player["position"],
+                    frontcourt_touches: player["frontcourt_touches"],
+                    elbow_touches: player["elbow_touches"],
+                    post_touches: player["post_touches"],
+                    paint_touches: player["paint_touches"],
+                    points_per_elbow_touch: player["points_per_elbow_touch"],
+                    points_per_post_touch: player["points_per_post_touch"],
+                    points_per_paint_touch: player["points_per_paint_touch"],
+                    points_per_fc_touch: fc_points_calc
+                    )
+
+    end
+  end
+end
